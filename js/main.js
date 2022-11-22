f906dc147feb18b307170526ca1cd408b9854972
marty-zhu <marty.zhu@gmail.com>
2022-11-22 15:34:12 -0500

    added function to create offer

--- js/main.js
+++ js/main.js
@@ -5,6 +5,20 @@ let peerConnection;
 let init = async () => {
     localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
     document.getElementById('user-1').srcObject = localStream;
+
+    createOffer();
 };
+
+let createOffer = async () => {
+    peerConnection = new RTCPeerConnection();
+
+    remoteStream = new MediaStream();
+    document.getElementById('user-2').srcObject = remoteStream;
+
+    let offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+    console.log('offer:', offer);
+}
 
 init();
